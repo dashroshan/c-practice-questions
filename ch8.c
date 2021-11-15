9f21e0ee7a3c1fe2a63c070072b006c26d287bdb
roshan1337d <roshan1337x@gmail.com>
2021-11-15 23:39:42 +0530

    upto ch8 q3

--- ch8.c
+++ ch8.c
@@ -13,3 +13,40 @@ int main()
 	return (0);
 }
 */
+
+/*Q8.2
+#include <stdio.h>
+int main()
+{
+	char name[20];
+	printf("Who is the inventor of ? : ");
+	for (int i = 1; i <= 3; i += 1)
+	{
+		gets(name);
+		if (strcmp(name, "Dennis Ritchie") == 0 || strcmp(name, "dennis ritchie") == 0)
+		{
+			printf("Good!");
+			break;
+		}
+		else if (i != 3)
+		{
+			printf("Try again : ");
+		}
+	}
+	return (0);
+}
+*/
+
+/*Q8.3
+#include <stdio.h>
+int main()
+{
+	char string[] = "sample text";
+	int m = 2, n = 3;
+	for (int i = 1; i <= m && n + i - 2 < strlen(string); i += 1)
+	{
+		printf("%c", string[n + i - 2]);
+	}
+	return (0);
+}
+*/
