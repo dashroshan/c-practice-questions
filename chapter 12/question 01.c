ad24737595e13a672b368122fec62b8abcfdeba5
Roshan Dash <83511412+roshan1337d@users.noreply.github.com>
2021-12-02 16:01:03 +0530

    switching data files to .o extension

--- chapter 12/question 01.c
+++ chapter 12/question 01.c
@@ -4,19 +4,19 @@ void copyContents(char *sourceFile, char *destinationFile);
 
 int main()
 {
-	copyContents("data","dataCopy");
-	return(0);
+	copyContents("data.o", "dataCopy.o");
+	return (0);
 }
 
 void copyContents(char *sourceFile, char *destinationFile)
 {
 	FILE *fpSource, *fpDestination;
 	char c;
-	fpSource=fopen(sourceFile,"r");
-	fpDestination=fopen(destinationFile,"w");
-	while((c=getc(fpSource))!=EOF)
+	fpSource = fopen(sourceFile, "r");
+	fpDestination = fopen(destinationFile, "w");
+	while ((c = getc(fpSource)) != EOF)
 	{
-		putc(c,fpDestination);
+		putc(c, fpDestination);
 	}
 	fclose(fpSource);
 	fclose(fpDestination);
